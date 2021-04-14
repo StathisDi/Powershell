5d96caea0f054a8d828754c13469b65baf838e9d
Dimitrios Stathis <stathis@kth.se>
2021-04-14 20:28:36 +0200

    slideshow wallpaper tested

--- others/wallpaper.ps1
+++ others/wallpaper.ps1
@@ -17,6 +17,24 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #>
 Param($location)
+function writeLog {
+  param ([string] $LOG, [string] $STR)
+  Write-Host $LOG
+  if (Test-Path $LOG -PathType Leaf) {
+    # File already exist
+    $date = (Get-Date -UFormat "%A %Y/%b/%d - %T : ")
+    $text = $date + $STR
+    $text | Out-File -FilePath $LOG -Append 
+  }
+  else {
+    $date = (Get-Date -UFormat "%A %Y/%b/%d - %T : ")
+    $text = $date + $STR
+    $text | Out-File -FilePath $LOG
+  }
+  
+}
+
+$log_file = "$HOME\Documents\wallpaper_log"
 
 if (!$location) {
   Write-Host "Location of the wallpaper folder is required!"
@@ -27,8 +45,20 @@ else {
     Write-Host "A Valid location to the wallpaper folder is required!"
     return -1
   }
+  if ((-not (Test-Path $log_file))) {
+    mkdir $log_file
+  }
+  $log_file = "$HOME\Documents\wallpaper_log\log.txt"
   $random = Get-Random $(Get-ChildItem -Path $location).Count
-  $image = Get-ChildItem -name -Path $location | Select-Object -index $(Get-Random $random)
-  Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $location\$image
+  $image = Get-ChildItem -name -Path $location | Select-Object -index $random
+  try {
+    Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $location\$image -ErrorAction Stop
+    writeLog $log_file $image
+    Write-Host $image
+  }
+  catch {
+    writeLog $log_file "ERROR"
+    return -1
+  }
   rundll32.exe user32.dll, UpdatePerUserSystemParameters
 }
